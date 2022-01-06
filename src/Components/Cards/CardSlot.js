34de97449d4a8e77a70ebbe3f826c6c3e442e89a
Ertuğrul Evrensel <ertugrulevrensel@gmail.com>
2022-01-06 22:15:44 +0300

    edit columns and functions

---
+++ src/Components/Cards/CardSlot.js
@@ -0,0 +1,126 @@
+import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
+import backFace from "../../assets/bf.webp";
+import "./cards.css";
+import React, { useState, useEffect } from "react";
+import { itemColumns, shuffle } from "../Deck";
+
+function onDragEnd(result, columns, setColumns) {
+  if (!result.destination) return;
+  const { source, destination } = result;
+  if (source.droppableId !== destination.droppableId) {
+    const sourceCol = columns[source.droppableId];
+    const destCol = columns[destination.droppableId];
+    const sourceItems = [...sourceCol.items];
+    const destItems = [...destCol.items];
+    const [removed] = sourceItems.splice(source.index, 1);
+    destItems.splice(destination.index, 0, removed);
+    setColumns({
+      ...columns,
+      [source.droppableId]: {
+        ...sourceCol,
+        items: sourceItems,
+      },
+      [destination.droppableId]: {
+        ...destCol,
+        items: destItems,
+      },
+    });
+  } else {
+    const column = columns[source.droppableId];
+    const copiedItems = [...column.items];
+    const [removed] = copiedItems.splice(source.index, 1);
+    copiedItems.splice(destination.index, 0, removed);
+    setColumns({
+      ...columns,
+      [source.droppableId]: {
+        ...column,
+        items: copiedItems,
+      },
+    });
+  }
+}
+
+function CardSlot() {
+  const [columns, setColumns] = useState(itemColumns);
+
+  useEffect(() => {
+    shuffle(columns, setColumns);
+  }, "");
+
+  return (
+    <div className="cardsArea">
+      <DragDropContext
+        onDragEnd={(result) => onDragEnd(result, columns, setColumns)}
+      >
+        {Object.entries(columns).map(([id, col]) => {
+          return (
+            <div
+              key={id}
+              style={{
+                display: "flex",
+                width: "100%",
+              }}
+            >
+              <Droppable droppableId={id}>
+                {(provided, snapshot) => {
+                  return (
+                    <div
+                      style={{
+                        background: snapshot.isDraggingOver
+                          ? "lightblue"
+                          : "lightgray",
+                        padding: 4,
+                        width: "100%",
+                        height: 500,
+                        margin: 10,
+                        justifyContent: "space-between",
+                        display: "grid",
+                        gridTemplateRows: "repeat(20,2rem)",
+                      }}
+                      {...provided.droppableProps}
+                      ref={provided.innerRef}
+                    >
+                      {col.items.map((item, index) => {
+                        return (
+                          <div key={item.id}>
+                            {item.isDrag ? (
+                              <Draggable
+                                draggableId={item.id.toString()}
+                                index={index}
+                              >
+                                {(provided, snapshot) => {
+                                  return (
+                                    <div
+                                      className="cardFront"
+                                      ref={provided.innerRef}
+                                      {...provided.draggableProps}
+                                      {...provided.dragHandleProps}
+                                    >
+                                      {item.card.name}
+                                    </div>
+                                  );
+                                }}
+                              </Draggable>
+                            ) : (
+                              <div className="cardBack">
+                                <img src={backFace} alt=""></img>
+                                {item.card.name}
+                              </div>
+                            )}
+                          </div>
+                        );
+                      })}
+                      {provided.placeholder}
+                    </div>
+                  );
+                }}
+              </Droppable>
+            </div>
+          );
+        })}
+      </DragDropContext>
+    </div>
+  );
+}
+
+export default CardSlot;
---
+++ src/Components/Deck/index.js
@@ -0,0 +1,74 @@
+const card = [
+  { rank: 1, name: "A" },
+  { rank: 2, name: "2" },
+  { rank: 3, name: "3" },
+  { rank: 4, name: "4" },
+  { rank: 5, name: "5" },
+  { rank: 6, name: "6" },
+  { rank: 7, name: "7" },
+  { rank: 8, name: "8" },
+  { rank: 9, name: "9" },
+  { rank: 10, name: "10" },
+  { rank: 11, name: "J" },
+  { rank: 12, name: "Q" },
+  { rank: 13, name: "K" },
+];
+export const itemColumns = {
+  0: { items: [] },
+  1: { items: [] },
+  2: { items: [] },
+  3: { items: [] },
+  4: { items: [] },
+  5: { items: [] },
+  6: { items: [] },
+  7: { items: [] },
+  8: { items: [] },
+  9: { items: [] },
+};
+export function shuffle(columns, setColumns) {
+  let deckEight = [];
+  let idCounter = 0;
+  for (let y = 0; y < card.length * 8; y++) {
+    deckEight.push({
+      id: idCounter,
+      card: card[y % 13],
+      isOpen: false,
+      isDrag: false,
+    });
+    idCounter = idCounter + 1;
+  }
+  let tmpCol = { ...itemColumns };
+  for (let i = 0; i < 104; i++) {
+    let random = Math.ceil(Math.random() * (103 - i));
+    if (i < 50) {
+      let tmpItems = tmpCol[i % 10];
+      let copiItem = [...tmpItems.items];
+      copiItem.splice(0, 0, deckEight[random]);
+
+      tmpCol = {
+        ...tmpCol,
+        [i % 10]: {
+          ...tmpItems,
+          items: copiItem,
+        },
+      };
+    } else if (i >= 50 && i < 54) {
+      let tmpItems = tmpCol[i % 4];
+      let copiItem = [...tmpItems.items];
+      copiItem.splice(0, 0, deckEight[random]);
+      tmpCol = {
+        ...tmpCol,
+        [i % 4]: {
+          ...tmpItems,
+          items: copiItem,
+        },
+      };
+    }
+    deckEight.splice(random, 1);
+  }
+  for (let r = 0; r < 10; r++) {
+    tmpCol[r].items[tmpCol[r].items.length - 1].isDrag = true;
+    tmpCol[r].items[tmpCol[r].items.length - 1].isOpen = true;
+  }
+  setColumns(tmpCol);
+}
